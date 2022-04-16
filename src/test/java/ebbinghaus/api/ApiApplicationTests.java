314b7e43c60ef6da69232a40e2bd18db05b31592
PPFei5Zhou <pengfei_zhou555@163.com>
2022-04-16 22:56:39 +0800

    first commit

---
+++ src/test/java/ebbinghaus/api/ApiApplicationTests.java
@@ -0,0 +1,30 @@
+package ebbinghaus.api;
+
+import ebbinghaus.api.core.DatabaseTestConfiguration;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Import(DatabaseTestConfiguration.class)
+class ApiApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void smokeTest() throws Exception {
+        mockMvc.perform(get("/actuator/health"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("UP"));
+    }
+
+}
---
+++ src/test/java/ebbinghaus/api/core/DatabaseTestConfiguration.java
@@ -0,0 +1,28 @@
+package ebbinghaus.api.core;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+import javax.sql.DataSource;
+
+public class DatabaseTestConfiguration {
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public MySQLContainer<?> mySqlContainer() {
+        return new MySQLContainer<>("mysql:8.0.28-debian").waitingFor(Wait.forListeningPort());
+    }
+
+    @Bean
+    @FlywayDataSource
+    public DataSource dataSource(MySQLContainer<?> mySqlContainer) {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(mySqlContainer.getJdbcUrl());
+        hikariConfig.setUsername(mySqlContainer.getUsername());
+        hikariConfig.setPassword(mySqlContainer.getPassword());
+        return new HikariDataSource(hikariConfig);
+    }
+}
